98f94618574bcd34d5afa495832be4e64532bef4
EC2 Default User <ec2-user@ip-172-31-17-167.us-east-2.compute.internal>
2021-12-09 18:07:58 +0000

    jquery 読み込めない

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){
+  $("p").css("color", "yellow");
+});
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211209093901) do
+ActiveRecord::Schema.define(version: 20211209105405) do
 
   create_table "cutmodels", force: :cascade do |t|
     t.string   "stylist"
