abfc42916ae0fcca71ffa4545e3455bc5949a41e
EC2 Default User <ec2-user@ip-172-31-17-167.us-east-2.compute.internal>
2021-12-12 06:06:40 +0000

    gemfile消えた

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,10 @@
 class PicturesController < ApplicationController
     def index
-        
+        @q = Cutmodel.ransack(params[:q])
+        @result = @q.result(distinct: true)
+        @result = @result.each_slice(3).to_a
+    end
+    
+    def show
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
     root to: "homes#index"
+    
+    get "/pictures", to: "pictures#index", as: :pictures
 end
